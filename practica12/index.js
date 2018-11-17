86ab4b5dc46fa5f693a0725216f8e123880b7eca
Jackson Huang <jacksonh201325@gmail.com>
2018-11-17 12:45:35 -0300

    Segundo

--- practica12/index.js
+++ practica12/index.js
@@ -1,9 +1,7 @@
 let lista = [11, 34, 87, 30, "otro"]
 
 recorreder = () => {
-    lista.forEach( element => {
-        console.log(element)
-    });
+    lista.forEach( element => console.log(element));
 }
-
+//Test change GIt
 recorreder();
